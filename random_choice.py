74e49d409aa0633c7bf1ada82268754d0c789512
nniconn <nicole.nguyen321@gmail.com>
2017-07-16 11:33:24 +0000

    Initial commit

---
+++ random_choice.py
@@ -0,0 +1,10 @@
+import random
+
+ingredients = ["gin", "vodka", "rum", "tequila"]
+
+style = ["old fashioned", "minimal", "bold", "light"]
+
+# Print the name of a random liquor
+
+print(random.choice(style)) 
+print(random.choice(ingredients))
